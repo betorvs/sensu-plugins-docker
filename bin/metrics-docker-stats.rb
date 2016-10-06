36626974501de852d91f64b8768a36cfd1eaa2c7
Roberto Scudeller <beto.rvs@gmail.com>
2016-10-06 10:06:58 -0300

    update metrics-docker-stats.rb with Update metric-docker-stats.rb from tata2000 user

--- bin/metrics-docker-stats.rb
+++ bin/metrics-docker-stats.rb
@@ -1,11 +1,11 @@
 #! /usr/bin/env ruby
 #
-#   metrics-docker-stats
+#   docker-container-metrics
 #
 # DESCRIPTION:
 #
 # Supports the stats feature of the docker remote api ( docker server 1.5 and newer )
-# Supports connecting to docker remote API over Unix socket or TCP
+# Currently only supports when docker is listening on tcp port.
 #
 #
 # OUTPUT:
@@ -18,16 +18,7 @@
 #   gem: sensu-plugin
 #
 # USAGE:
-#   Gather stats from all containers on a host using socket:
-#   metrics-docker-stats.rb -p unix -H /var/run/docker.sock
-#
-#   Gather stats from all containers on a host using TCP:
-#   metrics-docker-stats.rb -p http -H localhost:2375
-#
-#   Gather stats from a specific container using socket:
-#   metrics-docker-stats.rb -p unix -H /var/run/docker.sock -c 5bf1b82382eb
-#
-#   See metrics-docker-stats.rb --help for full usage flags
+#   #YELLOW
 #
 # NOTES:
 #
@@ -37,6 +28,7 @@
 #   for details.
 #
 
+require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/metric/cli'
 require 'socket'
 require 'net_http_unix'
@@ -69,10 +61,11 @@ class DockerStatsMetrics < Sensu::Plugin::Metric::CLI::Graphite
          default: ''
 
   option :docker_host,
-         description: 'Docker socket to connect. TCP: "host:port" or Unix: "/path/to/docker.sock" (default: "127.0.0.1:2375")',
+         description: 'location of docker api, host:port or /path/to/docker.sock',
          short: '-H DOCKER_HOST',
          long: '--docker-host DOCKER_HOST',
-         default: '127.0.0.1:2375'
+         default: '127.0.0.1:2375',
+         proc: proc { |v|  v.gsub('tcp://', '').gsub('unix://', '') }
 
   option :docker_protocol,
          description: 'http or unix',
@@ -90,11 +83,11 @@ class DockerStatsMetrics < Sensu::Plugin::Metric::CLI::Graphite
   def run
     @timestamp = Time.now.to_i
 
-    list = if config[:container] != ''
-             [config[:container]]
-           else
-             list_containers
-           end
+    if config[:container] != ''
+      list = [config[:container]]
+    else
+      list = list_containers
+    end
     list.each do |container|
       stats = container_stats(container)
       output_stats(container, stats)
@@ -106,57 +99,65 @@ class DockerStatsMetrics < Sensu::Plugin::Metric::CLI::Graphite
     dotted_stats = Hash.to_dotted_hash stats
     dotted_stats.each do |key, value|
       next if key == 'read' # unecessary timestamp
-      next if value.is_a?(Array)
+      next if key.start_with? 'blkio_stats' # array values, figure out later
       output "#{config[:scheme]}.#{container}.#{key}", value, @timestamp
     end
   end
 
-  def docker_api(path)
+  def docker_api(path, full_body = false)
     if config[:docker_protocol] == 'unix'
-      begin
-        NetX::HTTPUnix.start("unix://#{config[:docker_host]}") do |http|
-          request = Net::HTTP::Get.new "/#{path}"
-          http.request request do |response|
-            response.read_body do |chunk|
-              @response = JSON.parse(chunk)
-              http.finish
-            end
-          end
-        end
-      rescue NoMethodError
-        # using http.finish to prematurely kill the stream causes this exception.
-        return @response
+      request = Net::HTTP::Get.new "/#{path}"
+      NetX::HTTPUnix.start("unix://#{config[:docker_host]}") do |http|
+        get_response(full_body, http, request)
       end
     else
       uri = URI("#{config[:docker_protocol]}://#{config[:docker_host]}/#{path}")
-      begin
-        Net::HTTP.start(uri.host, uri.port) do |http|
-          request = Net::HTTP::Get.new uri.request_uri
-          http.request request do |response|
-            response.read_body do |chunk|
-              @response = JSON.parse(chunk)
-              http.finish
-            end
-          end
-        end
-      rescue NoMethodError
-        # using http.finish to prematurely kill the stream causes this exception.
-        return @response
+      request = Net::HTTP::Get.new uri.request_uri
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        get_response(full_body, http, request)
       end
     end
+  end
+
+  def get_response(full_body, http, request)
+    if full_body
+      get_full_response(http, request)
+    else
+      get_single_chunk(http, request)
+    end
+  end
+
+  def get_full_response(http, request)
+    http.request request do |response|
+      @response = JSON.parse(response.read_body)
+    end
+    http.finish
+    @response
+  end
+
+  def get_single_chunk(http, request)
+    http.request request do |response|
+      response.read_body do |chunk|
+        @response = JSON.parse(chunk)
+        http.finish
+      end
+    end
+    rescue NoMethodError
+      # using http.finish to prematurely kill the stream causes this exception.
+      return @response
   end
 
   def list_containers
     list = []
     path = 'containers/json'
-    @containers = docker_api(path)
+    @containers = docker_api(path, true)
 
     @containers.each do |container|
-      list << if config[:friendly_names]
-                container['Names'][0].delete('/')
-              else
-                container['Id']
-              end
+      if config[:friendly_names]
+        list << container['Names'][0].gsub('/', '')
+      else
+        list << container['Id']
+      end
     end
     list
   end
